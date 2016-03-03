1dbb1adbc69e9c7784322ca0c335a37b7af45685
David Monk <dm2614@ic.ac.uk>
2016-03-03 23:55:38 +0000

    PublicKey class commit

--- ConsoleApplication1/PublicKey.cs
+++ ConsoleApplication1/PublicKey.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace ConsoleApplication1
 {
     class PublicKey
     {
+        public BigInteger n;
+        public BigInteger e;
+        public PublicKey(BigInteger n, BigInteger e)
+        {
+            this.n = n;
+            this.e = e;
+        }
     }
 }
