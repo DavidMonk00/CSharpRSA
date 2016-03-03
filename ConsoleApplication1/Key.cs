8039f6b92edd4ea8c75fcfa592a98581b7aa68dd
David Monk <dm2614@ic.ac.uk>
2016-03-03 23:52:58 +0000

    First Commit

---
+++ ConsoleApplication1/Key.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace RSA
+{
+    public class Key
+    {
+        private int strength;
+        private BigInteger start;
+        private BigInteger start_second;
+        private BigInteger p;
+        private BigInteger q;
+        private BigInteger n;
+        private BigInteger phi_n;
+        public int key_length;
+        private BigInteger e = new BigInteger(65537);
+        private BigInteger d;
+        //public PublicKey pubkey;
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Numerics;
+
+namespace RSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i < 35 ; i++)
+            {
+                if (PrimeCheck.Check(new BigInteger(Math.Pow(2, i)-1)))
+                {
+                    Console.WriteLine(Math.Pow(2, i)-1);
+                }
+            }
+            //Console.ReadLine();
+        }
+    }
+}
